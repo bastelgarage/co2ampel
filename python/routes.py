0bc5cbb5d06ee8a443641968ebe14ad871529b2a
Fabian Affolter <mail@fabian-affolter.ch>
2020-11-19 23:13:06 +0100

    Add template support

--- python/routes.py
+++ python/routes.py
@@ -1,7 +1,8 @@
-from views import index, co2ampel_handler
+from views import co2ampel_handler, index, index_json
 
 
 def setup_routes(app):
     """Handle the routes."""
     app.router.add_get("/", index)
+    app.router.add_get("/json", index_json)
     app.router.add_post("/co2ampel", co2ampel_handler)
--- python/views.py
+++ python/views.py
@@ -1,14 +1,21 @@
 import hashlib
-import time
 import os
+import time
 
+import aiohttp_jinja2
 from aiohttp import web
 
 sensors = {}
 
 
+@aiohttp_jinja2.template("index.html")
 async def index(request):
     """Serve the content for the index."""
+    return {"sensors": sensors}
+
+
+async def index_json(request):
+    """Serve the content as JSON."""
     return web.json_response(sensors)
 
 
